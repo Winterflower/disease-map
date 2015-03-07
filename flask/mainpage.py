f5f081e32a5391159081fd8067131033b351e5be
Eleonore9 <eleonore.mayola@ymail.com>
2015-03-07 13:57:12 +0000

    Done: csv upload works

--- flask/mainpage.py
+++ flask/mainpage.py
@@ -1,4 +1,5 @@
 __author__ = 'winterflower'
+import os
 from flask import Flask, render_template, request, redirect, url_for
 app = Flask(__name__)
 
@@ -13,27 +14,32 @@ def visualization():
 
 
 # Global variables for CSV file upload
-UPLOAD_FOLDER = '/uploads'
+UPLOAD_FOLDER = '/home/eleonore/Documents/fb_hack_2015/disease-map/flask/uploads'
 ALLOWED_EXTENSIONS = set(['csv'])
+app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
 
 # Check allowed extensions:
 def allowed_file(filename):
     return '.' in filename and \
         filename.rsplit('.', 1)[1] in ALLOWED_EXTENSIONS
 
-@app.route('/upload_page')
+@app.route('/upload_page', methods=['GET', 'POST'])
 def upload_page():
     return render_template("test_upload.html")
 
+
 @app.route('/upload_csv', methods=['GET', 'POST'])
-def upload_file():
+def upload_csv():
     if request.method == 'POST':
         file = request.files['file']
+        print "*" * 3
         if file and allowed_file(file.filename):
-            filename = secure_filename(file.filename)
+            filename = file.filename
+            print os.path.join(app.config['UPLOAD_FOLDER'], filename)
             file.save(os.path.join(app.config['UPLOAD_FOLDER'], filename))
-            return redirect(url_for('uploaded_file',
-                                    filename=filename))
+            
+            #return redirect(url_for('uploaded_file', filename=filename))
+    return '''    '''
 
 
 
