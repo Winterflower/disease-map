a3137a73d736928f85c4475f0dabf0c57b522d9c
Eleonore9 <eleonore.mayola@ymail.com>
2015-03-07 12:23:47 +0000

    In progess: csv upload + added Flask Debug

--- flask/mainpage.py
+++ flask/mainpage.py
@@ -1,6 +1,7 @@
 __author__ = 'winterflower'
-from flask import Flask, render_template
+from flask import Flask, render_template, request, redirect, url_for
 app = Flask(__name__)
+
 
 @app.route('/')
 def hello_world():
@@ -10,11 +11,31 @@ def hello_world():
 def visualization():
     return "Now generating your visualization"
 
-@app.route('/upload_csv')
-def upload_csv():
+
+# Global variables for CSV file upload
+UPLOAD_FOLDER = '/uploads'
+ALLOWED_EXTENSIONS = set(['csv'])
+
+# Check allowed extensions:
+def allowed_file(filename):
+    return '.' in filename and \
+        filename.rsplit('.', 1)[1] in ALLOWED_EXTENSIONS
+
+@app.route('/upload_page')
+def upload_page():
     return render_template("test_upload.html")
+
+@app.route('/upload_csv', methods=['GET', 'POST'])
+def upload_file():
+    if request.method == 'POST':
+        file = request.files['file']
+        if file and allowed_file(file.filename):
+            filename = secure_filename(file.filename)
+            file.save(os.path.join(app.config['UPLOAD_FOLDER'], filename))
+            return redirect(url_for('uploaded_file',
+                                    filename=filename))
 
 
 
 if __name__ == '__main__':
-    app.run()
+    app.run(debug=True)
